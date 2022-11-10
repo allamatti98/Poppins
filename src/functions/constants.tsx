dbece8caeafd24a15f99b51b9daf0aa1463f0a4a
allamatti98 <dayanallamatti98@gmail.com>
2022-11-10 19:14:28 +0300

    Poppins sorted

---
+++ src/functions/constants.tsx
@@ -0,0 +1,7 @@
+export const projectId = process.env.REACT_APP_PROJECT_ID
+  ? process.env.REACT_APP_PROJECT_ID
+  : "05563c5e-ccbb-41fd-bb1f-bbf5cdd83272";
+
+export const privateKey: string = process.env.REACT_APP_PROJECT_KEY
+  ? process.env.REACT_APP_PROJECT_KEY
+  : "a227835e-7c7d-467b-92db-ac54acbef502";
